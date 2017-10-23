7ab9ddd00d67280e938b627ea00d0ebd23b3919e
Marcus Renno <renno.marcus@gmail.com>
2017-10-23 19:43:20 -0400

    put assert inside test module

--- tests/unit/module-import.js
+++ tests/unit/module-import.js
@@ -1,8 +1,8 @@
 import { test, module } from 'ember-qunit';
 import hljs from 'highlight';
 
-module('Shim for highlightjs');
-
-test('the import works', function(assert) {
-  assert.ok(hljs);
+module('Shim for highlightjs', function() {
+  test('the import works', function(assert) {
+    assert.ok(hljs);
+  });
 });
